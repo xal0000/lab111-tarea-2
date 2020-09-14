b014f370fbfc3783ed73e8a0346629dc81c0102e
xal0000 <70548283+xal0000@users.noreply.github.com>
2020-09-14 00:35:46 -0400

    alex jhonny perez torrez
    ci:13757498
    lab111-tarea2

---
+++ tarea.py
@@ -0,0 +1,12 @@
+numero1=int(input("ingrese el numero a:"))
+numero2=int(input("ingrese el numero b:"))
+
+if numero1 > numero2:
+    print("el numero:",numero1,"es mayor")
+if numero2 > numero1:
+    print("el numero:",numero2,"es mayor")
+if numero1 == numero2:
+    print("los dos numeros son iguales")        
+
+
+    
